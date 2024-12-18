305fd0c8ed955119c416eb71b5f3a2f7f0220403
nazzz1324 <ovovkopo@gmail.com>
2024-12-18 16:54:01 +0500

    Commit 2

---
+++ ElementGizulin/Gizulin.py
@@ -0,0 +1,21 @@
+class ElementGizulin:
+    def __init__(self, name='Гелий', symbol='He', number='2'):
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
+    @property
+    def name(self):
+        return self.__name
+    @property
+    def symbol(self):
+        return self.__symbol
+    @property
+    def number(self):
+        return self.__number
+    def dump(self):
+        print(f"Имя элемента: {self.name}")
+        print(f"Символ: {self.symbol}")
+        print(f"Атомный номер: {self.number}")
+if __name__ == "__main__":
+    element = ElementGizulin()
+    element.dump()
---
+++ ElementValeev/Valeev.py
@@ -0,0 +1,21 @@
+class ElementValeev:
+    def __init__(self, name='', symbol='', number=''):
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
+    @property
+    def name(self):
+        return self.__name
+    @property
+    def symbol(self):
+        return self.__symbol
+    @property
+    def number(self):
+        return self.__number
+    def dump(self):
+        print(f"Имя элемента: {self.name}")
+        print(f"Символ: {self.symbol}")
+        print(f"Атомный номер: {self.number}")
+if __name__ == "__main__":
+    element = ElementValeev()
+    element.dump()
